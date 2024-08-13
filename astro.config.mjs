308a19139b1e9c0a5850a5e30dd4a793c05f02e5
harris <msca5238@gmail.com>
2024-08-13 10:18:57 +0800

    finish learn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 设置网站唯一Netlify URL
+  site: "https://mayflyblog.netlify.app/",
+  integrations: [preact()]
+});
